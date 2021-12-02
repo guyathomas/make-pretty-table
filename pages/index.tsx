90ded4b7d1ff539b75424023adc2a3ee70d4ef40
Guy Thomas <guythomas721@gmail.com>
2021-12-01 19:41:16 -0800

    MVP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,82 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-import { GetStaticProps } from 'next'
+import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Layout from "../components/layout";
 
-export default function Home({
-  allPostsData
-}: {
-  allPostsData: {
-    date: string
-    title: string
-    id: string
-  }[]
-}) {
+const listToMatrix = (list: any[], subArraySize: number) =>
+  list.reduce(
+    (rows, key, index) =>
+      (index % subArraySize == 0
+        ? rows.push([key])
+        : rows[rows.length - 1].push(key)) && rows,
+    []
+  );
+
+export default function Home() {
+  const [numRows, setNumRows] = React.useState(2);
+  const [input, setInput] = React.useState("");
+  const [output, setOutput] = React.useState<string[][]>([]);
+  const handleFormat = (input: string) =>
+    listToMatrix(input.split("\n"), numRows);
+
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+    <Layout>
+      <Box
+        component="form"
+        padding={1}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "25rem",
+        }}
+        noValidate
+        autoComplete="off"
+        onSubmit={(event) => {
+          event.preventDefault();
+          setOutput(handleFormat(input));
+        }}
+      >
+        <TextField
+          type="number"
+          onChange={(event) => setNumRows(parseInt(event.target.value, 10))}
+          variant="outlined"
+          sx={{ marginTop: 1 }}
+          label="How many rows?"
+          value={numRows}
+        />
+        <TextField
+          variant="outlined"
+          label="input"
+          sx={{ marginTop: 1 }}
+          multiline
+          maxRows={10}
+          minRows={5}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <Button sx={{ marginTop: 1 }} variant="contained" type="submit">
+          Create
+        </Button>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableBody>
+            {output.map((row) => (
+              <TableRow>
+                {row.map((cell) => (
+                  <TableCell>{cell}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Layout>
-  )
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
+  );
 }
